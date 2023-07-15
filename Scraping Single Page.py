d497235bac8d1727334fb91f63b6e0a8de58f270
rites221 <119439984+rites221@users.noreply.github.com>
2023-07-15 21:53:41 +0530

    Update Scraping Single Page.py

--- Scraping Single Page.py
+++ Scraping Single Page.py
@@ -9,9 +9,7 @@ result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content,'lxml')
 # print(soup.prettify())
-#
-# #how to scrape a single page
-#
+
 box = soup.find('article', class_='main-article')
 
 links = []
