e5e0f9c397f5ce5651f71d48f5e213c1957c7f70
rites221 <119439984+rites221@users.noreply.github.com>
2023-07-22 16:34:12 +0530

    Update Scraping Single Page.py

--- Scraping Single Page.py
+++ Scraping Single Page.py
@@ -3,6 +3,27 @@
 from bs4 import BeautifulSoup
 import requests
 
+#how to scrap a single page
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+
+box = soup.find('article', class_='main-article')
+
+title = box.find('h1').get_text()
+
+transcript = box.find('div', class_='full-script').get_text()
+
+print(title)
+print(transcript)
+
+
+#HOW TO SCRAPE MULTIPLE LINKS IN A SINGLE PAGE
+
+
 root = 'https://subslikescript.com'
 website = f'{root}/movies'
 result = requests.get(website)
@@ -31,5 +52,3 @@ for link in links:
 
     with open(f'{title}.txt','w',encoding='utf-8') as file:
         file.write(transcript)
-
-
